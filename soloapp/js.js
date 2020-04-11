99407333b17069c00acfd3ea0533494720190cab
Abdul Majid <abdulmajidred@gmail.com>
2020-04-11 20:39:36 +0530

    soloapp created

---
+++ soloapp/js.js
@@ -0,0 +1,5 @@
+// Created by Abhi
+
+alert("Welcome! \n \n Create Fake SOLOLEARN chat messages and prank your friends!");
+
+alert("If you are new then please go through INSTRUCTION using SETTINGS button and read them!")
